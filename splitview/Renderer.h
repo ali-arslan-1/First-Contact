964a1cffa7edf6f5ad1b7939133bdb311d28607a
Ali Arslan <arslan.ale@gmail.com>
2015-06-13 22:15:47 +0200

    Renderer added.

---
+++ splitview/Renderer.h
@@ -0,0 +1,13 @@
+//
+//  Renderer.h
+//  splitview
+//
+//  Created by Ali Arslan on 13.06.15.
+//  Copyright (c) 2015 Ming Li. All rights reserved.
+//
+
+#ifndef splitview_Renderer_h
+#define splitview_Renderer_h
+
+
+#endif
---
+++ splitview/Renderer.m
@@ -0,0 +1,9 @@
+//
+//  Renderer.m
+//  splitview
+//
+//  Created by Ali Arslan on 13.06.15.
+//  Copyright (c) 2015 Ming Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
