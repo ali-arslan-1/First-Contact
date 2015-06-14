3128a6cbacd254b11643b3153be479c711840d31
Ali Arslan <arslan.ale@gmail.com>
2015-06-14 15:26:37 +0200

    Restructured engine code.

---
+++ splitview/UniformContainer.h
@@ -0,0 +1,13 @@
+//
+//  UniformContainer.h
+//  splitview
+//
+//  Created by Ali Arslan on 14.06.15.
+//  Copyright (c) 2015 Ming Li. All rights reserved.
+//
+
+#ifndef splitview_UniformContainer_h
+#define splitview_UniformContainer_h
+
+
+#endif
---
+++ splitview/UniformContainer.m
@@ -0,0 +1,9 @@
+//
+//  UniformContainer.m
+//  splitview
+//
+//  Created by Ali Arslan on 14.06.15.
+//  Copyright (c) 2015 Ming Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
